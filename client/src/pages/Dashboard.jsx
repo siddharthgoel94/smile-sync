2c8cbaa3d510c675cab47e8ca549bf58aa273be4
TeekshaHarish <102624456+TeekshaHarish@users.noreply.github.com>
2024-04-13 00:26:45 +0530

    Questionaire

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,69 +1,200 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import "../styles/Dashboard.css";
-import { Link, useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import axios from 'axios';
-import questionBank from '../../utils/QuestionBank';
-
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import axios from "axios";
+import questionBank from "../../utils/QuestionBank";
 
 const Dashboard = () => {
-  const [ token, setToken ] = useState(JSON.parse(localStorage.getItem("auth")) || "");
-  const [ data, setData ] = useState({});
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+
+  const handleNextQuestion = () => {
+    setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+    console.log(currentQuestionIndex);
+  };
+
+  const handlePrevQuestion = () => {
+    setCurrentQuestionIndex((prevIndex) => prevIndex - 1);
+  };
+  // const ans = new Array(questionBank.anxietyQuestions.length);
+  const n = questionBank.anxietyQuestions.length; // Desired length of the array
+  const [answers, setAnswers] = useState(new Array(n).fill(0)); // Initialize array with zeros
+
+  const handleSubmit = () => {
+    // Handle submission logic here
+    console.log("Submit Answers");
+    // for (let i = 0; i < answers.length; i++) {
+    //   console.log(answers[i]);
+    // }
+    console.log(answers);
+  };
+
+  // const updateIndex = (index, value) => {
+  //   setAnswers((prevArray) => {
+  //     const newArray = [...prevArray]; // Create a copy of the array
+  //     newArray[index] = value; // Update the value at the specified index
+  //     return newArray; // Return the updated array
+  //   });
+  // };
+  const handleRadioClick = (index, value) => {
+    setAnswers((prevArray) => {
+      const newArray = [...prevArray]; // Create a copy of the array
+      newArray[index] = value; // Update the value at the specified index
+      return newArray; // Return the updated array
+    });
+    // ans[index] = value;
+    // console.log(index, value, ans[index]);
+  };
+
+  const [token, setToken] = useState(
+    JSON.parse(localStorage.getItem("auth")) || ""
+  );
+
+  const [data, setData] = useState({});
   const navigate = useNavigate();
 
   const fetchLuckyNumber = async () => {
-
     let axiosConfig = {
       headers: {
-        'Authorization': `Bearer ${token}`
-    }
+        Authorization: `Bearer ${token}`,
+      },
     };
 
     try {
-      const response = await axios.get("http://localhost:3000/api/v1/dashboard", axiosConfig);
+      const response = await axios.get(
+        "http://localhost:3000/api/v1/dashboard",
+        axiosConfig
+      );
       setData({ msg: response.data.msg, luckyNumber: response.data.secret });
     } catch (error) {
       toast.error(error.message);
     }
-  }
+  };
 
-
-  
   useEffect(() => {
     fetchLuckyNumber();
-    if(token === ""){
+    if (token === "") {
       navigate("/login");
       toast.warn("Please login first to access dashboard");
     }
   }, [token]);
 
   return (
-    <div className='dashboard-main'>
+    <div className="dashboard-main">
       <h1>Dashboard</h1>
-      <p>{ data.msg }! Lets start with a questionnaire to know you better</p>
-      <form>
-      {
-        questionBank.anxietyQuestions.map((question,index)=>(
-           <div key={index}>
-        {question.question}
-          <div class="btn-group mx-4" role="group" aria-label="Basic radio toggle button group" required>
-  <input type="radio" class="btn-check" name="btnradio" id={`btnradio${index}-yes`} autocomplete="off"/>
-  <label class="btn btn-outline-primary" for={`btnradio${index}-yes`}>Yes</label>
+      <p>{data.msg}! Lets start with a questionnaire to know you better</p>
+      {/* <form> */}
+      {/* {questionBank.anxietyQuestions.map((question, index) => (
+          <div key={index}>
+            {question.question}
+            <div
+              class="btn-group mx-4"
+              role="group"
+              aria-label="Basic radio toggle button group"
+              required
+            >
+              <input
+                type="radio"
+                class="btn-check"
+                name="btnradio"
+                id={`btnradio${index}-yes`}
+                autocomplete="off"
+              />
+              <label
+                class="btn btn-outline-primary"
+                for={`btnradio${index}-yes`}
+              >
+                Yes
+              </label>
 
-  <input type="radio" class="btn-check" name="btnradio" id={`btnradio${index}-no`} autocomplete="off"/>
-  <label class="btn btn-outline-secondary" for={`btnradio${index}-no`}>No</label>
-</div>
-</div>
-        ))
-        
-      }
-      <div className='mx-auto'>
-      <button className='btn btn-success w-50 mx-auto' type="submit">Submit my Answers</button>
-      </div>
-      </form>
-      <Link to="/logout" className="logout-button">Logout</Link>
+              <input
+                type="radio"
+                class="btn-check"
+                name="btnradio"
+                id={`btnradio${index}-no`}
+                autocomplete="off"
+              />
+              <label
+                class="btn btn-outline-secondary"
+                for={`btnradio${index}-no`}
+              >
+                No
+              </label>
+            </div>
+          </div>
+        ))} */}
+      {currentQuestionIndex < questionBank.anxietyQuestions.length ? (
+        <div>
+          <h2>Question {currentQuestionIndex + 1}</h2>
+          <p>{questionBank.anxietyQuestions[currentQuestionIndex].question}</p>
+          <div
+            className="btn-group mx-4"
+            role="group"
+            aria-label="Basic radio toggle button group"
+            required
+          >
+            <input
+              type="radio"
+              className="btn-check"
+              name={`btnradio-${currentQuestionIndex}`}
+              id={`btnradio${currentQuestionIndex}-yes`}
+              onClick={() => handleRadioClick(currentQuestionIndex, 1)}
+              // autocomplete="off"
+            />
+            <label
+              className="btn btn-outline-primary"
+              htmlFor={`btnradio${currentQuestionIndex}-yes`}
+            >
+              Yes
+            </label>
+
+            <input
+              type="radio"
+              className="btn-check"
+              name={`btnradio-${currentQuestionIndex}`}
+              id={`btnradio${currentQuestionIndex}-no`}
+              onClick={() => handleRadioClick(currentQuestionIndex, 0)}
+              // autocomplete="off"
+            />
+            <label
+              className="btn btn-outline-secondary"
+              htmlFor={`btnradio${currentQuestionIndex}-no`}
+            >
+              No
+            </label>
+          </div>
+          <button
+            onClick={handlePrevQuestion}
+            disabled={currentQuestionIndex === 0}
+          >
+            Previous
+          </button>
+          <button
+            onClick={handleNextQuestion}
+            disabled={
+              currentQuestionIndex === questionBank.anxietyQuestions.length
+            }
+          >
+            Next
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h2>Thank you for completing the quiz!</h2>
+          <button onClick={handleSubmit}>Submit</button>
+        </div>
+      )}
+      {/* <div className="mx-auto">
+        <button className="btn btn-success w-50 mx-auto" type="submit">
+          Submit my Answers
+        </button>
+      </div> */}
+      {/* </form> */}
+      <Link to="/logout" className="logout-button">
+        Logout
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
