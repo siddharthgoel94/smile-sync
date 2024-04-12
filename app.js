aa45659de5cb24de348d74052cd4de9aa1f14249
TeekshaHarish <102624456+TeekshaHarish@users.noreply.github.com>
2024-04-13 01:19:03 +0530

    data modification

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const express = require("express");
 const cors = require('cors')
 const app = express();
 const mainRouter = require("./routes/user");
+const http = require('http');
+const socketIo = require('socket.io');
+const server = http.createServer(app);
+const io = socketIo(server);
 
 app.use(express.json());
 
@@ -18,7 +22,7 @@ const start = async () => {
 
     try {        
         await connectDB(process.env.MONGO_URI);
-        app.listen(port, () => {
+        server.listen(port, () => {
             console.log(`Server is listening on port ${port}`);
         })
 
